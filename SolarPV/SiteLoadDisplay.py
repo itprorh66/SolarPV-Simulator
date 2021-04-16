itprorh66/SolarPV-Simulator

commit 7049bafebf2945393902954313f1e72b4cf9a6a0

Site Load update
 Address Issues #10, 12, & 13 related to improving Site Load Definition performance and ease of use

SolarPV/SiteLoadDisplay.py
@@ -3,7 +3,8 @@
 """
 Created on Wed May 30 12:04:12 2018
 Modified on 02/22/2019 for version 0.1.0
-
+Modified on 04/11/2021 to address Issues #10, 12, & 13 related to improving 
+            Site Load Definition performance and ease of use
 @author: Bob Hentz
 
 
@@ -20,17 +21,31 @@ Modified on 02/22/2019 for version 0.1.0
               or FITNESS FOR A PARTICULAR PURPOSE.
  -------------------------------------------------------------------------------
 """
-from tkinter import *
+import tkinter as tk
 import tkinter.ttk as ttk
-from SiteLoad import *
+from SiteLoad import SiteLoad
 
-#TODO Correct intemittent input data gathering
-#TODO Implement on combo block content change, update watts field
-#TODO implement record delete functionality
+
+class table_button_cell(ttk.Button):
+    """ Creates a Button Cell """
+    def __init__(self, parent, tblpos, datpos, text, callback):
+        self.tblpos = tblpos
+        self.datpos = datpos
+        self.text = text
+        self.width = 5
+        self.callback = callback
+        ttk.Button.__init__(self, parent, text= self.text , 
+                            command= self.button_resp)
+        self.grid(row=self.tblpos[0], column=self.tblpos[1], padx=2, pady=2,
+                     sticky= (tk.E, tk.W))        
+    def button_resp(self):
+        if self.callback:
+            self.callback(self.tblpos, self.datpos)
+    
 class table_combo_cell(ttk.Combobox):
     """ Creates a Selector Cell to contain Type Selection Options """
     def __init__(self, parent, tblpos, datpos, var, opts_lst, 
-                 validate_command= None):
+                 select_command= None):
         self.tblpos = tblpos
         self.datpos = datpos
         self.opts = opts_lst
@@ -39,38 +54,21 @@ class table_combo_cell(ttk.Combobox):
             if len(li) > self.width:
                 self.width = len(li)
         self.var = var
-        self.evar = StringVar()
+        self.evar = tk.StringVar()
         self.evar.set(str(self.var))
         self.parent = parent
-        self.validate_command = validate_command
-        self.validate = None
-        if self.validate_command:
-            self.validate = 'focusout'     
-        ttk.Combobox.__init__(self, parent, values=self.opts, textvariable= self.evar, 
-                               width = self.width, exportselection= False, 
-                               validate= self.validate, 
-                               validatecommand= self.is_okay)
+        self.select_command = select_command
+        ttk.Combobox.__init__(self, parent, values=self.opts, 
+                              textvariable= self.evar, 
+                               width = self.width, exportselection= False)
         self.grid(row=self.tblpos[0], column=self.tblpos[1], padx=2, pady=2,
-                     sticky= (E, W))
-        self.bind('<FocusOut>', self.event_process)
-        self.bind('<Return>', self.event_process)
-        self.bind('<Key-Delete>', self.delete)
-
-    def event_process(self, event):
-        self.is_okay()
-               
-    def delete(self, event):
-        #TODO Add delete row function to Site Load Display
-        # Ask if you want to delete the record
-        # If yes, drop row by index number, else do nothing
+                     sticky= (tk.E, tk.W))
+        self.bind("<<ComboboxSelected>>", self.is_selected)
         
-        pass
-
-    def is_okay(self):
-        """ Vailidate contents of entry using method passed by caller """
-        new_val = self.evar.get()
-        if new_val != self.var:
-            return self.validate_command(self.datpos, new_val)   
+    def is_selected(self, event):
+        if self.select_command:
+            self.select_command(self.tblpos, self.datpos, self.get())
+        
        
 class table_data_cell(ttk.Entry):
     """ Creates an Enrty cell to contain table data """
@@ -80,23 +78,23 @@ class table_data_cell(ttk.Entry):
         self.datpos = datpos
         self.varTyp = type(var)
         self.var = var
-        self.evar = StringVar()
+        self.evar = tk.StringVar()
         self.evar.set(str(self.var))
-        self.just = CENTER
+        self.just = tk.CENTER
         if self.datpos[1] == 0:
-            self.just = LEFT
+            self.just = tk.LEFT
         cw = 5
         if len(str(var))-2 > cw:
             cw =len(str(var)) +2
         ttk.Entry.__init__(self, parent, textvar= self.evar, width= cw, 
                            justify= self.just)
         self.grid(row=self.tblpos[0], column=self.tblpos[1], padx=2, pady=2, 
-                  sticky=(E, W))
-        self.bind('<Leave>', self.update) 
-        self.bind('<Key-Delete>', self.delete)
+                  sticky=(tk.E, tk.W))
+        self.bind('<Leave>', self.update)   # Mouse leaves cell
+        self.bind('FocusOut>', self.update) # Keyboard Focus exits
         self.bind('Key-Tab>', self.update)
-        self.bind('FocusOut>', self.update)
         self.bind('Key-Return>', self.update)
+
         
     def update(self, event):
         try:
@@ -120,68 +118,110 @@ class header_cell(ttk.Label):
         if len(var)-2 > cw:
             cw = len(var) +2
         ttk.Label.__init__(self, parent, text=var, font='Helvetica 10 bold', 
-                           borderwidth=2, width=cw+2, anchor=CENTER) 
-        self.grid(row= 0, column= colpos, padx= 3, pady= 2, sticky= (E ,W))
+                           borderwidth=2, width=cw+2, anchor=tk.CENTER) 
+        self.grid(row= 0, column= colpos, padx= 3, pady= 2, sticky= (tk.E , tk.W))
 
 
 class Table(ttk.Frame):
     """ Creates a master container to hold the DataFrame widgets """
-    def __init__(self, sldf, parent):
+    def __init__(self, sldf, parent, Delete_col = None):
         self.df = sldf
         self.col_hds = self.df.get_headers()
         self.LoadOpts = self.df.getTypeOptions()
         self.parent= parent
+        self.add_Del_col = Delete_col
         ttk.Frame.__init__(self,parent)
-        self.grid(sticky= (N, S, E, W))
+        self.grid(sticky= (tk.N, tk.S, tk.E,tk. W))
         self.dsply_table()
         
     def build_workArray(self):
+        #Load wa from underlying dataframe
         self.wa = list()
         for r in range(self.df.get_row_count()):
             self.wa.append(self.df.get_row_by_index(r))
+        #add extra row at end, for new entry
         self.wa.append(['']*len(self.col_hds))
     
     def dsply_table(self):
         self.build_workArray()
         tbl_row = 0
-        self.build_row(self.col_hds, 0, 0,True)
+        self.build_row(self.col_hds, 0, 0, TBLHDR=True)
         tbl_row += 2
         for r in range(len(self.wa)):
-            self.build_row(self.wa[r], r, tbl_row, False)
+            self.build_row(self.wa[r], r, tbl_row, TBLHDR=False)
             tbl_row += 2
 
     def build_row(self, val_list, data_row, tbl_row, TBLHDR=False):
-        lastrow = False
+        # lastrow = False
         if len(val_list) > 0:
-            for i in range(len(val_list)):
-                colpos = 1+i*2
-                ttk.Separator(self, orient=VERTICAL).grid(row=tbl_row, column = colpos-1, sticky= (N, S))               
+            for itm_no, itm_name, in enumerate(val_list):
+                colpos = 1 + itm_no*2
+                ttk.Separator(self, orient=tk.VERTICAL).grid(row=tbl_row, 
+                                 column = colpos-1, sticky= (tk.N, tk.S)) 
                 if TBLHDR:
-                    cf = header_cell(self, colpos, val_list[i])
+                     header_cell(self, colpos, itm_name)
+                                                
                 else:
-                    cf = self.define_data_cell([tbl_row, colpos],  [data_row, i], val_list[i])
-        ttk.Separator(self, orient=VERTICAL).grid(row=tbl_row+1, column = len(val_list)*2, sticky=(N, S)) 
+                    self.define_data_cell([tbl_row, colpos],  
+                                          [data_row, itm_no], itm_name)
+                ttk.Separator(self, orient=tk.VERTICAL).grid(row=tbl_row+1, 
+                     column = len(val_list)*2, sticky=(tk.N, tk.S))     
+        
+        if self.add_Del_col:
+            # Add the Delete Row column entries if required
+            if TBLHDR:
+                header_cell(self, colpos, self.add_Del_col)
+                
+            else:
+                table_button_cell(self, [tbl_row, colpos],  
+                                   [data_row, itm_no], 
+                                    'X', self.delete_row)   
+            ttk.Separator(self, orient=tk.VERTICAL).grid(row=tbl_row+1, 
+                 column = len(val_list)*2, sticky=(tk.N, tk.S))                         
 
     def define_data_cell(self, tblpos, datpos, var):
         if datpos[1] == 0:
-            cf = table_combo_cell(self, tblpos, datpos, var, self.LoadOpts, self.update_data)
+            table_combo_cell(self, tblpos, datpos, var, self.LoadOpts, 
+                             select_command= self.combo_selected)
         else:
-            cf = table_data_cell(self, tblpos,  datpos, var)          
+            table_data_cell(self, tblpos,  datpos, var)          
                        
-    def update_data(self, pos, new_val):
-        self.wa[pos[0]][pos[1]] = new_val
-        if pos[1] == 0:
-            ar = self.wa[pos[0]]
+    def delete_row(self, tpos, dpos):
+        """ Delete a row after delete button pushed """
+        if dpos[0] < self.df.get_row_count():
+            self.df.delete_row(dpos[0])
+            self.dsply_table()
+    
+    def combo_selected(self, tblpos, dpos, newval):
+        # Process a Combobox entry change 
+        if dpos[0] < self.df.get_row_count():
+            if self.wa[dpos[0]][dpos[1]] != newval:
+                ar = self.wa[dpos[0]]
+                ar[0] = newval
+                stdar = self.df.getDefaultRowValues(newval)
+                for ky, val in stdar.items():
+                    ar[self.col_hds.index(ky)] = val
+                self.wa[dpos[0]] = ar
+                self.df.update_row_values(dpos[0], ar)
+                self.dsply_table()
+        else:
+            # This is a new row 
+            ar = self.wa[dpos[0]]
+            ar[0] = newval
             ar = self.df.setStdRowValues(ar)
-            self.wa[pos[0]] = ar   
-        if pos[0] == len(self.wa) -1:
-            ar = []
-            for i in range(len(self.wa[pos[1]])):
-                ar.append(self.wa[pos[0]][i])
+            self.wa[dpos[0]] = ar
             self.df.add_new_row(ar)
             self.dsply_table()
-        else:
-            self.df.set_cell_value(pos, new_val)
+        
+    def update_data(self, dpos, new_val):
+        ## This is where we come after each change to a data entry cell 
+        ar = self.wa[dpos[0]]
+        if ar[dpos[1]] != new_val:
+            # A value change has occurred
+            ar[dpos[1]] = new_val
+            self.wa[dpos[0]] = ar
+            self.df.set_cell_value(dpos, new_val)
+            self.dsply_table()
 
     def on_form_close(self):
         pass
@@ -194,10 +234,10 @@ def main():
 
     print ('Starting Display')
     print (sl.get_dataframe())
-    root = Tk()        
+    root = tk.Tk()        
     
     root.title("Base Frame Testing")
-    tabframe = Table(sl, root)
+    Table(sl, root, "Del")
     root.mainloop()
     print('\nExiting Display')
     print (sl.get_dataframe())

SolarPV/DataFrame.py
@@ -5,7 +5,8 @@ Created on Mon Jun  4 19:17:27 2018
 Modified   Sat Dec  1 2018 (fix save/import issue)
 Modified   Tue Dec  4 2018 (fix Load Import Error Issue #11)
 Modified on 02/22/2019 for version 0.1.0
-
+Modified on 04/11/2021 to address Issues #10, 12, & 13 related to improving 
+            Site Load Definition performance and ease of use
 
 @author: Bob Hentz
 
@@ -48,7 +49,7 @@ class DataFrame:
     def delete_row(self, rwid):
         """ Delete a row from the dataframe """
         assert rwid < self.df.shape[0]
-        self.df.drop(self.df.index[rwid])
+        self.df.drop(self.df.index[rwid], inplace=True)
         
     def set_cell_value(self, pos, val):
         """ Update a DataFrame cell value pos = [row,col]  """
@@ -62,6 +63,14 @@ class DataFrame:
                 self.df.at[row, self.col_hds[col]] = self.col_typs[col](val)
             return True
         return False
+
+    def update_row_values(self, rwid, val_list):
+        """ Update the contents of the frame row  """
+        assert rwid < self.df.shape[0]
+        for colid, val in enumerate(val_list):
+            self.set_cell_value([rwid, colid], val)
+        
+
 
     def get_row_count(self):
         """ Return the row size of the DataFrame """

SolarPV/SPVSwbrd.py
@@ -4,6 +4,7 @@
 Created on Fri Jun  8 14:42:27 2018
 Modified on 11/27/2018 to Clean up Comments
 Modified on 02/22/2019 for version 0.1.0
+Modified 0n 04/11/2021 to implement Record delete function see issue #13
 @author: Bob Hentz
 -------------------------------------------------------------------------------
   Name:        SPVSwbDisplay.py
@@ -57,7 +58,7 @@ class spvSwitchboard(tbf.switchboard):
     def dsplyLoad(self):
         """ Display Site Electric Load Definition Input Form """
         self.define_toplevel( 'Electrical Load')
-        self.frm = sld.Table(self.src.load, self.dsply)
+        self.frm = sld.Table(self.src.load, self.dsply, "Del")
            
     def dsplyBats(self):
         """ Display Battery Description Input Form """

SolarPV/SiteLoad.py
@@ -5,6 +5,8 @@ Created on Wed May  2 13:15:35 2018
 Modified   Sat Dec  1 2018 (fix save/import issue)
 Modified   Wed Dec  5 2018 (Fix Issue 2, Handle DC Loads)
 Modified on 02/22/2019 for version 0.1.0
+Modified on 04/11/2021 to address Issues #10, 12, & 13 related to improving 
+            Site Load Definition performance and ease of use
 
 @author: Bob Hentz
 
@@ -24,8 +26,7 @@ Modified on 02/22/2019 for version 0.1.0
 import numpy as np
 import pandas as pd
 import Parameters as sp
-from DataFrame import *
-import matplotlib.pyplot as plt
+from DataFrame import DataFrame
 import guiFrames as tbf
 
 def findindex(val):
@@ -45,13 +46,16 @@ class SiteLoad(DataFrame):
         self.master = master
         DataFrame.__init__(self,   sp.load_fields, sp.load_field_types)
 
-
-    def addRow(self, typ, qty=None, uf=None, hrs=None, st=0, wts=None, src=None, mde=None):
+    def addRow(self, typ, qty=None, uf=None, hrs=None, st=0, wts=None, 
+               mde=None):
         """ Create a new entry in the load array from individual items """
-        ar = [typ, qty, uf, hrs, st, wts, src, mde]
-        print (ar)
+        ar = [typ, qty, uf, hrs, st, wts, mde]
         self.add_new_row(ar)
         
+    def getDefaultRowValues(self, load_type):
+        """ Return the dictionary of default values for this type """
+        return sp.load_types[load_type]
+    
     def setStdRowValues(self, ar):
         """ Update AR based on change of Load Type """
         if ar[0] != '':
@@ -134,7 +138,7 @@ class SiteLoad(DataFrame):
             pltlist = [{'label': 'Load', 'data': np.array(elp['Total']),
                             'type': 'Bar', 'color': 'grey', 'width': 0.4,
                             'xaxis':np.array([x for x in range(24)])}]
-            dp = tbf.plot_graphic(window, 'Hour of Day', 'Watts',
+            tbf.plot_graphic(window, 'Hour of Day', 'Watts',
                                   np.array([x for x in range(24)]),
                         pltlist,'Hourly Electrical Use Profile', (6,4),
                         text_inserts= [pl,tdl,avhl])
@@ -173,6 +177,7 @@ def main():
     sl.add_new_row(['Well Pump DC, 1 HP', 1, 0.35, 12, 8, 500.0, 'DC'])
     sl.add_new_row(['Phone Charger', 10, 0.45, 12, 6, 2.0, 'DC'])
     elp = sl.get_load_profile()
+    print(elp)
     print(sl.get_dataframe())
 
 

SolarPV/guiFrames.py
@@ -3,6 +3,9 @@
 """
 Created on Tue Jul 10 11:07:02 2018
 Modified on 02/22/2019 for version 0.1.0
+Modified on 04/11/2021 to address Issues #10, 12, & 13 related to improving 
+            Site Load Definition performance and ease of use
+
 
 @author: Bob Hentz
 
@@ -20,16 +23,13 @@ Modified on 02/22/2019 for version 0.1.0
  -------------------------------------------------------------------------------
 """
 
-from tkinter import *
-import matplotlib
-#TODO Investigat whether this can come out or is it truly necessary
-#matplotlib.use('TkAgg', warn=False, force=True)
+
+import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-
-from tkinter.messagebox import *
+from tkinter.messagebox import askyesno
 import tkinter.ttk as ttk
-from tkinter.filedialog import *
+from tkinter.filedialog import askopenfilename 
 
 
 """  Helper Methods & Functions """
@@ -42,7 +42,7 @@ def ask_question(title, mssg, **kargs):
 
 def show_warning(title, mssg):
     """ Implements a pop-up Window that Notifies the User of an error/warning """
-    showwarning(title, mssg)
+    tk.showwarning(title, mssg)
 
 def cleanse_data(data):
     """ Converts a null data display to None Value """
@@ -61,9 +61,9 @@ def popup_notification(parent, message, command, *command_args, color= '#ffff80'
     """ Create a popup notification to alert user to long running process
         Execute the Process, clear the alert screen and return the process results """
     rslts = None
-    tplvl = Toplevel()
+    tplvl = tk.Toplevel()
     tplvl.lift()
-    lbl = Label(tplvl, text = message, padx= 5, pady=5, bd= 5, bg= color)
+    lbl = tk.Label(tplvl, text = message, padx= 5, pady=5, bd= 5, bg= color)
     lbl.pack(fill = "both", expand=True)
     parent.update_idletasks()
     if command_args is None:
@@ -84,7 +84,7 @@ def build_menubar(parent, menu_itms):
     def add_sub_menu(mnu, cmd_dict):
         for ky in cmd_dict.keys():
             if len(cmd_dict[ky]) > 0:
-                mi = Menu(mnu, tearoff= 0)
+                mi = tk.Menu(mnu, tearoff= 0)
                 mnu.add_cascade(label=ky, menu=mi)
                 for itm in cmd_dict[ky]:
                     if type(itm) == tuple:
@@ -92,7 +92,7 @@ def build_menubar(parent, menu_itms):
                     else:
                         add_sub_menu(mi, itm)
             
-    mbar = Menu(parent)
+    mbar = tk.Menu(parent)
     add_sub_menu(mbar, menu_itms)
     parent.config(menu=mbar)
     return mbar
@@ -169,7 +169,7 @@ class plot_graphic(ttk.Frame):
         ttk.Frame.__init__(self, self.parent)
         self.grid(row= 0, column= 0, sticky = "NWES", padx=5, pady=5)
         for i in range(len(text_inserts)):
-            tx = ttk.Label(self, text=text_inserts[i]).grid(row = i+1, column= 0, sticky = 'EW')
+            ttk.Label(self, text=text_inserts[i]).grid(row = i+1, column= 0, sticky = 'EW')
         self.fig = make_figure(xlabel, ylabel, xaxis, yaxislist, pltlbl, pltsize, **kargs)
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
         self.canvas.draw()
@@ -194,7 +194,7 @@ class list_cell(ttk.Combobox):
         if location is not None:
             self.loc = location
         self.wdth = None
-        self.val = StringVar()
+        self.val = tk.StringVar()
         self.val.set(self.src.read_data())
         self.validate = None
         self.chg_cmd = On_change
@@ -207,8 +207,8 @@ class list_cell(ttk.Combobox):
                            textvariable=self.val,
                            values= list(filter(lambda x: x.startswith(self.val.get()), 
                                                self.opts)),
-                           exportselection = 0, justify= LEFT)
-        self.grid(row= self.loc[0], column= self.loc[1], sticky=(E))
+                           exportselection = 0, justify= tk.LEFT)
+        self.grid(row= self.loc[0], column= self.loc[1], sticky=(tk.E))
         self.bind('<FocusOut>', self.is_okay)
         self.bind('<KeyRelease>', self.on_chg)
       
@@ -247,7 +247,7 @@ class data_cell(ttk.Entry):
         self.loc = [0,1]
         if location is not None:
             self.loc = location
-        self.val = StringVar()
+        self.val = tk.StringVar()
         self.val.set(self.src.read_data())
         self.validate = None
         self.chg_cmd = On_change
@@ -257,11 +257,11 @@ class data_cell(ttk.Entry):
     def show_frame(self):
         """ Display The Frame """       
         ttk.Entry.__init__(self, self.parent, width=self.min_width, 
-                           textvariable=self.val, justify= CENTER 
+                           textvariable=self.val, justify= tk.CENTER 
                            #validate= self.validate, 
                            #validatecommand= self.validate_cmd
                            )
-        self.grid(row= self.loc[0], column= self.loc[1], sticky=(EW))
+        self.grid(row= self.loc[0], column= self.loc[1], sticky=(tk.EW))
         self.bind('<FocusOut>', self.is_okay)
         self.bind('<KeyRelease>', self.on_chg)
       
@@ -290,7 +290,7 @@ class data_cell(ttk.Entry):
         return rslt
         
 
-class note_cell(Text):
+class note_cell(tk.Text):
     """ Creates an Entry Field Widget for input & update of Long string data  """
     def __init__(self, parent_frame, data_src, size= None, location= None): 
         self.parent = parent_frame
@@ -301,17 +301,17 @@ class note_cell(Text):
         self.size = [1, 50]
         if size != None:
             self.size = size
-        Text.__init__(self, self.parent, height= self.size[0], width= self.size[1],
-                      wrap= WORD, padx = 5, pady = 5)
-        self.grid(row= self.loc[0], column= self.loc[1], sticky=(N, S, W, E))
+        tk.Text.__init__(self, self.parent, height= self.size[0], width= self.size[1],
+                      wrap= tk.WORD, padx = 5, pady = 5)
+        self.grid(row= self.loc[0], column= self.loc[1], sticky=(tk.N, tk.S, tk.W, tk.E))
         self.insert('1.0', self.src.read_data())
         self.bind('<FocusOut>', self.on_chg)
 
     def is_dirty(self):
-        return self.src.read_data() != self.get('1.0', END+'-1c')
+        return self.src.read_data() != self.get('1.0', tk.END+'-1c')
 
     def get_val(self):
-        return self.get('1.0', END+'-1c')
+        return self.get('1.0', tk.END+'-1c')
 
     def on_chg(self, event):
         self.src.write_data(self.get_val())
@@ -343,32 +343,41 @@ class entry_form_frame(ttk.Frame):
        
    def make_frame(self):
         ttk.Frame.__init__(self, self.parent)
-        self.grid(row = self.loc[0], column= self.loc[1], sticky= (N, W, E, S), columnspan=self.colspan)
+        self.grid(row = self.loc[0], column= self.loc[1], 
+                  sticky= (tk.N, tk.W, tk.E, tk.S), columnspan=self.colspan)
         if self.frm == "Data Entry":
-            ttk.Label(self, padding='2 2 2 2', text=self.src.get_label()).grid(column=0, row=0, sticky=(W, E))
+            ttk.Label(self, padding='2 2 2 2', 
+                      text=self.src.get_label()).grid(column=0, row=0, 
+                                                      sticky=(tk.W, tk.E))
             self._ntry = data_cell(self, self.src, min_lngth=self.size, location= [0, 1],
                       validate_command= self.validate_cmd, On_change= self.chg_cmd) 
         elif self.frm == "List Entry":
-            ttk.Label(self, padding='2 2 2 2', text=self.src.get_label()).grid(column=0, row=0, sticky=(W, E))
-            self._ntry = list_cell(self, self.src, min_lngth=self.size, location= [0, 1],
+            ttk.Label(self, padding='2 2 2 2', 
+                      text=self.src.get_label()).grid(column=0, row=0, 
+                                                      sticky=(tk.W, tk.E))
+            self._ntry = list_cell(self, self.src, min_lngth=self.size, 
+                                   location= [0, 1],
                       validate_command= self.validate_cmd, On_change= self.chg_cmd)
         else:
-            ttk.Label(self, padding='2 2 2 2', text=self.src.get_label()).grid(column=0, row=0, sticky=(W, E))
-            self._ntry = note_cell(self, self.src, size=self.size, location= [1, 0])
+            ttk.Label(self, padding='2 2 2 2', 
+                      text=self.src.get_label()).grid(column=0, row=0, 
+                                                      sticky=(tk.W, tk.E))
+            self._ntry = note_cell(self, self.src, size=self.size, 
+                                   location= [1, 0])
                   
         
     
 """ Master Frame Classes Used to Implement GUI Functions  """
-class status_window(LabelFrame):
+class status_window(tk.LabelFrame):
     """ Implements a Status Window and Manages display of status messages """
     def __init__(self, parent, title, loc= None, spn = None ):
         self.parent = parent
         if loc is None:
             loc = [0,0]
-        LabelFrame.__init__(self, parent, text= title) 
+        tk.LabelFrame.__init__(self, parent, text= title) 
         self.grid(row= loc[0], column= loc[1], columnspan= spn, sticky = 'EW')
         self.configure(borderwidth= 5, width= 500, height= 50, 
-                       relief= GROOVE, background= '#ccffb3')
+                       relief= tk.GROOVE, background= '#ccffb3')
         self.lbl = None
         
     def show_message(self, message, style= None):
@@ -387,7 +396,7 @@ class status_window(LabelFrame):
            s = 'Notice: ' + s
         if self.lbl is not None:
             self.lbl.destroy()
-        self.lbl = Label(self, text= s, font= fs, padx= 2, pady= 2, 
+        self.lbl = tk.Label(self, text= s, font= fs, padx= 2, pady= 2, 
                             background= '#ccffb3', foreground= fg)
         self.lbl.grid(row= 0, column=0)
         self.parent.update_idletasks()
@@ -406,8 +415,9 @@ class switchboard(ttk.LabelFrame):
             self.menuTitle = menuTitle
         self.mstrKey = None
         ttk.LabelFrame.__init__(self, self.parent, text=self.menuTitle, borderwidth= 5,
-                                width= 400, height = 500, padding= 5, relief= GROOVE)
-        self.grid(row = self.loc[0], column = self.loc[1], sticky = ( N, S, W, E))
+                                width= 400, height = 500, padding= 5, relief= tk.GROOVE)
+        self.grid(row = self.loc[0], column = self.loc[1], 
+                  sticky = ( tk.N, tk.S, tk.W, tk.E))
         self.grid_propagate(0)
         self.define_menu()
         self.display_actions()
@@ -428,12 +438,12 @@ class switchboard(ttk.LabelFrame):
         for act in self.actions:
 #            menu_action(act[0], act[1], [cur_row, cur_col], parent = self)
             ttk.Label(self, text = act[0], padding= '2 5 2 2').grid(
-                    row = cur_row, column= 1, sticky= (E, W))
+                    row = cur_row, column= 1, sticky= (tk.E, tk.W))
             ttk.Label(self, text = " ", padding= '2 5 2 2').grid(
-                    row = cur_row, column= 2, sticky= (E, W))
+                    row = cur_row, column= 2, sticky= (tk.E, tk.W))
             ttk.Button(self, text= act[1], command = act[2], 
                        padding= '2 5 2 2').grid(row = cur_row, 
-                                         column =3, sticky= (E, W))            
+                                         column =3, sticky= (tk.E, tk.W))            
             cur_row += 1
             
 

commit 1fd17702014976e9d5bbd4368307478915009706

Fixed Missing Mode Header Column
Issue was the mode column in the load entry table was missing. This issue was first introduced in 7049bafe when Bob was adding logic for a delete column in load entry table.

SolarPV/SiteLoadDisplay.py
@@ -147,6 +147,7 @@ class Table(ttk.Frame):
 
     def build_row(self, val_list, data_row, tbl_row, TBLHDR=False):
         # lastrow = False
+        colpos = 0
         if len(val_list) > 0:
             for itm_no, itm_name, in enumerate(val_list):
                 colpos = 1 + itm_no*2
@@ -158,20 +159,21 @@ class Table(ttk.Frame):
                 else:
                     self.define_data_cell([tbl_row, colpos],  
                                           [data_row, itm_no], itm_name)
-                ttk.Separator(self, orient=tk.VERTICAL).grid(row=tbl_row+1, 
-                     column = len(val_list)*2, sticky=(tk.N, tk.S))     
+            ttk.Separator(self, orient=tk.VERTICAL).grid(row=tbl_row+1,
+                 column = colpos+1, sticky=(tk.N, tk.S))
         
-        if self.add_Del_col:
-            # Add the Delete Row column entries if required
-            if TBLHDR:
-                header_cell(self, colpos, self.add_Del_col)
-                
-            else:
-                table_button_cell(self, [tbl_row, colpos],  
-                                   [data_row, itm_no], 
-                                    'X', self.delete_row)   
-            ttk.Separator(self, orient=tk.VERTICAL).grid(row=tbl_row+1, 
-                 column = len(val_list)*2, sticky=(tk.N, tk.S))                         
+            if self.add_Del_col:
+                # Add the Delete Row column entries if required
+                colpos+=2
+                if TBLHDR:
+                    header_cell(self, colpos, self.add_Del_col)
+
+                else:
+                    table_button_cell(self, [tbl_row, colpos],
+                                       [data_row, itm_no],
+                                        'X', self.delete_row)
+                ttk.Separator(self, orient=tk.VERTICAL).grid(row=tbl_row+1,
+                     column = colpos+1, sticky=(tk.N, tk.S))
 
     def define_data_cell(self, tblpos, datpos, var):
         if datpos[1] == 0: